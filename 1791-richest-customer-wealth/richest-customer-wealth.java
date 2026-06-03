c17e44fb0de120bd15b7765f093c87feb04cb5f1
Ruchika Rahate <158438150+Ruchika220@users.noreply.github.com>
2026-06-04 01:41:08 +0530

    Time: 0 ms (100.00%) | Memory: 44.7 MB (17.76%) - LeetSync

---
+++ 1791-richest-customer-wealth/richest-customer-wealth.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maximumWealth(int[][] arr) {
+    
+        
+        int max=0;
+
+        for(int i = 0; i < arr.length; i++){
+            int sum=0;
+            for(int j = 0; j< arr[i].length; j++ ){
+                sum +=arr[i][j];
+                //return sum;
+                }
+                if(sum>max){
+                    max = sum;
+            }
+        }
+        return max;
+    }
+}

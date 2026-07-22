f29d068772c88d9f8f7d8a0967f862ba570b3f81
Ruchika Rahate <158438150+Ruchika220@users.noreply.github.com>
2026-07-22 23:13:06 +0530

    Time: 0 ms (100.00%) | Memory: 44 MB (11.17%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,14 +1,57 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int n = nums.length;
-        int i=0;
-        for( i = 0; i<n; i++)
-{
-    if(nums[i] == target){
-     return i;
-    }
-}        
 
-return -1;
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target)
+                return mid;
+
+            // Left half is sorted
+            if (nums[left] <= nums[mid]) {
+
+                if (target >= nums[left] && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+
+            }
+            // Right half is sorted
+            else {
+
+                if (target > nums[mid] && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+
+        return -1;
     }
 }
+
+
+
+
+
+
+// class Solution {
+//     public int search(int[] nums, int target) {
+//         int n = nums.length;
+//         int i=0;
+//         for( i = 0; i<n; i++)
+// {
+//     if(nums[i] == target){
+//      return i;
+//     }
+// }        
+
+// return -1;
+//     }
+// }
